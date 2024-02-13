723211269532f3b07cffa739f5ee4f07a5de442f
Kiryl Litoshka <kirill.litoshko@gmail.com>
2024-02-13 10:16:27 +0300

    updated main manu styles

--- clients/warmth/src/pages/home/Home.jsx
+++ clients/warmth/src/pages/home/Home.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import "./Home.css"
 import {Link} from "react-router-dom";
 
-const Home = () => {
+const main = () => {
     return (
-        <div className="home">
-            <div className="home-menu">
-                <div className="home-menu_item">
-                    <Link to="/objects" className="home-menu_item-link">Объекты</Link>
+        <div className="main">
+            <div className="main-menu">
+                <div className="main-menu_item">
+                    <Link to="/objects" className="main-menu_item-link">Объекты</Link>
                 </div>
-                <div className="home-menu_item">
-                    <Link to="/renters" className="home-menu_item-link">Арендаторы</Link>
+                <div className="main-menu_item">
+                    <Link to="/renters" className="main-menu_item-link">Арендаторы</Link>
+                </div>
+                <div className="main-menu_item">
+                    <Link to="/reports" className="main-menu_item-link">Отчеты</Link>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Home;
+export default main;

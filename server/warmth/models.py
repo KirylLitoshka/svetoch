af26615a9b5b24fcaf1fdc761f398613e97ecfa4
Kiryl Litoshka <kirill.litoshko@gmail.com>
2024-03-13 15:26:41 +0300

    updated renters and payments models (also added field description)

--- server/warmth/models.py
+++ server/warmth/models.py
@@ -78,6 +78,27 @@ objects = sa.Table(
     sa.Column("vat", sa.Float, default=0)
 )
 
+"""
+Таблица арендаторов
+name - краткое наименование арендатора
+full_name - полное наименование арендатора
+bank_id - реф на идентификатор банка
+banking_account - банковский счет
+registration_number - УНП
+respondent_number - ОКПО
+contract_number - номер договора
+contract_date - дата заключения договора
+is_bank_payer - для выставления счета в банк
+address - адрес арендатора
+contacts - контакты арендатора
+email - почта арендатора
+is_public_sector - является ли гос.организацией
+is_heating_available - доступно ли отопление
+heating_load - нагрузка по отоплению
+is_water_heating_available - доступно ли горячее водоснабжение
+water_heating_load - нагрузка по горячему водоснабжению
+is_closed - статус арендатора (закрыт ли?)
+"""
 renters = sa.Table(
     "renters", metadata,
     sa.Column("id", sa.Integer, primary_key=True),
@@ -85,7 +106,7 @@ renters = sa.Table(
     sa.Column("full_name", sa.String, nullable=False),
     sa.Column("bank_id", sa.Integer, sa.ForeignKey(
         "banks.id", ondelete="SET NULL"), nullable=True),
-    sa.Column("checking_account", sa.String, nullable=True),
+    sa.Column("banking_account", sa.String, nullable=True),
     sa.Column("registration_number", sa.String, nullable=True),
     sa.Column("respondent_number", sa.String, nullable=True),
     sa.Column("contract_number", sa.String, nullable=True),
@@ -93,6 +114,7 @@ renters = sa.Table(
     sa.Column("is_bank_payer", sa.Boolean, nullable=False),
     sa.Column("address", sa.String, nullable=True),
     sa.Column("contacts", sa.String, nullable=True),
+    sa.Column("email", sa.String, nullable=True),
     sa.Column("is_public_sector", sa.Boolean, default=False),
     sa.Column("is_heating_available", sa.Boolean, default=False),
     sa.Column("heating_load", sa.Float, nullable=True),
@@ -108,12 +130,30 @@ renters_objects = sa.Table(
     sa.Column("object_id", sa.Integer, sa.ForeignKey("objects.id", ondelete="CASCADE"))
 )
 
-
+"""
+Таблица начислений
+operation_month - месяц операционного периода
+operation_year - год операционного периода
+payment_month - месяц на который идет платеж
+payment_год - год на который идет платеж
+object_id - реф на обьект начисления
+payment_type - тип платежа (1 - основной, 2 - хз что это, 3 - перерасчет, остальное в помойку)
+ncen - номер цены (для жилых помещений, нужно ли вообще?)
+applied_rate_value - значение применяемого тарифа
+heating_value - количество гкал по отоплению
+heating_cost - стоимость отопления
+water_heating_value - количество гкал по горячему водоснабжению
+water_heating_cost - стоимость отопления
+is_additional_coefficient_applied - применение другого валютного коэффициента
+additional_coefficient_value - значение другого валютного коэффициента 
+"""
 payments = sa.Table(
     "payments", metadata,
     sa.Column("id", sa.Integer, primary_key=True),
-    sa.Column("month", sa.Integer, nullable=False),
-    sa.Column("year", sa.Integer, nullable=False),
+    sa.Column("operation_month", sa.Integer, nullable=False),
+    sa.Column("operation_year", sa.Integer, nullable=False),
+    sa.Column("payment_month", sa.Integer, nullable=False),
+    sa.Column("payment_year", sa.Integer, nullable=False),
     sa.Column("object_id", sa.Integer, sa.ForeignKey("objects.id", ondelete="CASCADE")),
     sa.Column("payment_type", sa.Integer, nullable=False),
     sa.Column("ncen", sa.Integer, nullable=False),
@@ -121,7 +161,9 @@ payments = sa.Table(
     sa.Column("heating_value", sa.Float, nullable=False),
     sa.Column("heating_cost", sa.Float, nullable=False),
     sa.Column("water_heating_value", sa.Float, nullable=False),
-    sa.Column("water_heating_cost", sa.Float, nullable=False)
+    sa.Column("water_heating_cost", sa.Float, nullable=False),
+    sa.Column("is_additional_coefficient_applied", sa.Boolean, default=False),
+    sa.Column("additional_coefficient_value", sa.Float, nullable=True)
 )
 
 workshops_groups = sa.Table(

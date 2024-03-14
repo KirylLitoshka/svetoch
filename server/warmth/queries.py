6c2f836358f8c7ef9a1d4645a93eed62ea2a8681
Kiryl Litoshka <kirill.litoshko@gmail.com>
2024-03-14 15:05:34 +0300

    updated queries

--- server/warmth/queries.py
+++ server/warmth/queries.py
@@ -14,7 +14,7 @@ __all__ = [
 ]
 
 
-async def get_renters_payments(conn, month, year, is_detailed=False):
+async def get_renters_payments(conn, month, year, is_detailed=False, is_bank_payment=False):
     stmt = select(
         renters.c.id, renters.c.name.label("renter_title"),
         func.sum(payments.c.heating_value).label("heating_value"),
@@ -38,8 +38,15 @@ async def get_renters_payments(conn, month, year, is_detailed=False):
         )).order_by(objects.c.id).alias("objects_payment")
         stmt = select(
             renters,
+            banks.c.title.label("bank_title"),
+            banks.c.code.label("bank_code"),
             func.json_agg(text("objects_payment.*")).label("includes")
-        ).select_from(renters.join(renters_objects).join(objects_payment)).group_by(renters.c.id).order_by(renters.c.id)
+        ).select_from(
+            renters.join(renters_objects).join(banks, isouter=True).join(objects_payment)
+        )
+        if is_bank_payment:
+            stmt = stmt.where(renters.c.is_bank_payer)
+        stmt = stmt.group_by(renters.c.id, banks).order_by(renters.c.id)
     cursor = await conn.execute(stmt)
     result = [dict(row) for row in cursor.fetchall()]
     if not result:
@@ -47,7 +54,7 @@ async def get_renters_payments(conn, month, year, is_detailed=False):
     return result
 
 
-async def get_total_values_by_workshop(conn, app_info):
+async def get_total_values_by_workshop(conn, month, year):
     cursor = await conn.execute(
         select(
             workshops.c.title,
@@ -65,8 +72,8 @@ async def get_total_values_by_workshop(conn, app_info):
                 payments, onclause=payments.c.object_id == objects.c.id
             )
         ).where(and_(
-            payments.c.month == app_info['month'],
-            payments.c.year == app_info['year'],
+            payments.c.month == month,
+            payments.c.year == year,
             payments.c.payment_type == 1,
             payments.c.ncen != 0
         )).group_by(workshops.c.id, workshops_groups.c.title).order_by(workshops.c.id)
@@ -77,11 +84,11 @@ async def get_total_values_by_workshop(conn, app_info):
     return result
 
 
-async def get_current_currency_coefficient(conn, app_info):
+async def get_current_currency_coefficient(conn, month, year):
     cursor = await conn.execute(
         select(currency_coefficients).where(and_(
-            currency_coefficients.c.month == app_info['month'],
-            currency_coefficients.c.year == app_info['year']
+            currency_coefficients.c.month == month,
+            currency_coefficients.c.year == year
         ))
     )
     result = cursor.fetchone()
@@ -107,8 +114,8 @@ async def get_reconciliation_codes_with_total(conn, month, year):
             func.sum(payments.c.water_heating_value).label("water_heating_value"),
             func.sum(payments.c.water_heating_cost).label("water_heating_cost")
         ).select_from(reconciliation_codes.join(objects).join(payments)).where(and_(
-            payments.c.month == month,
-            payments.c.year == year,
+            payments.c.operation_month == month,
+            payments.c.operation_year == year,
             payments.c.payment_type == 1,
             payments.c.ncen != 0
         )).group_by(reconciliation_codes.c.id).order_by(reconciliation_codes.c.id)
@@ -149,8 +156,8 @@ async def get_payments_info_by_workshop(conn, workshop_id, month, year):
     return result
 
 
-async def get_detail_by_workshop(conn, workshop_id, app_info):
+async def get_detail_by_workshop(conn, workshop_id, month, year):
     workshop = await get_workshop_by_id(conn, workshop_id)
-    payment_data = await get_payments_info_by_workshop(conn, workshop_id, app_info["month"], app_info["year"])
+    payment_data = await get_payments_info_by_workshop(conn, workshop_id, month, year)
     workshop["payments"] = payment_data
     return workshop

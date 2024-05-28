92ce02b7b85ba1cef8e07ca866e3ca97c6319105
Kiryl Litoshka <kirill.litoshko@gmail.com>
2024-05-28 16:13:46 +0300

    updated app initialization

--- server/electricity/app.py
+++ server/electricity/app.py
@@ -1,12 +1,13 @@
 from aiohttp import web
 from electricity.routes import routes
-from utils import on_shutdown, on_startup
+from utils import on_shutdown, on_startup, init_app_context_data
 
 
 def electricity_app():
     application = web.Application()
     application["app_name"] = "electricity"
     application.on_startup.append(on_startup)
+    application.on_startup.append(init_app_context_data)
     application.on_shutdown.append(on_shutdown)
     application.add_routes(routes)
     return application
--- server/warmth/app.py
+++ server/warmth/app.py
@@ -1,6 +1,6 @@
 from aiohttp import web
 
-from utils import on_shutdown, on_startup
+from utils import on_shutdown, on_startup, init_app_context_data
 from warmth.routes import routes
 
 
@@ -8,6 +8,7 @@ def warmth_app():
     application = web.Application()
     application["app_name"] = "warmth"
     application.on_startup.append(on_startup)
+    application.on_startup.append(init_app_context_data)
     application.on_shutdown.append(on_shutdown)
     application.add_routes(routes)
     return application

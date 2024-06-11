400d76c99b9e966f4c25851473ccdac47b3e6cca
Kiryl Litoshka <kirill.litoshko@gmail.com>
2024-06-11 16:12:04 +0300

    updated search component for renters and added search for renter selection

--- clients/warmth/src/forms/renter/RenterSearch.jsx
+++ clients/warmth/src/forms/renter/RenterSearch.jsx
@@ -4,48 +4,57 @@ import Search from "../../components/wrappers/search/Search";
 const RenterSearch = ({ searchQuery, setSearchQuery }) => {
   return (
     <Search>
-      <div className="search_row">
-        <label htmlFor="search_title">Наименование</label>
-        <input
-          id="search_title"
-          type="text"
-          value={searchQuery.title}
-          onChange={(e) =>
-            setSearchQuery({ ...searchQuery, title: e.target.value })
-          }
-        />
-      </div>
-      <div className="search_row">
-        <label htmlFor="search_code">УНП</label>
-        <input
-          id="search_code"
-          type="text"
-          value={searchQuery.code}
-          onChange={(e) =>
-            setSearchQuery({
-              ...searchQuery,
-              registration_number: e.target.value,
-            })
-          }
-        />
-      </div>
-      <div className="search_row">
-        <label htmlFor="search_closed">Статус арендатора</label>
-        <select
-          name="search_closed"
-          id="search_closed"
-          value={searchQuery.is_closed ? "1" : "0"}
-          onChange={(e) =>
-            setSearchQuery({
-              ...searchQuery,
-              is_closed: Boolean(+e.target.value),
-            })
-          }
-        >
-          <option value="0">Открыт</option>
-          <option value="1">Закрыт</option>
-        </select>
-      </div>
+      {searchQuery.hasOwnProperty("title") &&
+      (
+        <div className="search_row">
+          <label htmlFor="search_title">Наименование</label>
+          <input
+            id="search_title"
+            type="text"
+            value={searchQuery.title}
+            onChange={(e) =>
+              setSearchQuery({ ...searchQuery, title: e.target.value })
+            }
+          />
+        </div>
+      )}
+      {searchQuery.hasOwnProperty("registration_number") &&
+      (
+        <div className="search_row">
+          <label htmlFor="search_code">УНП</label>
+          <input
+            id="search_code"
+            type="text"
+            value={searchQuery.code}
+            onChange={(e) =>
+              setSearchQuery({
+                ...searchQuery,
+                registration_number: e.target.value,
+              })
+            }
+          />
+        </div>
+      )}
+      {searchQuery.hasOwnProperty("is_closed") &&
+      (
+        <div className="search_row">
+          <label htmlFor="search_closed">Статус арендатора</label>
+          <select
+            name="search_closed"
+            id="search_closed"
+            value={searchQuery.is_closed ? "1" : "0"}
+            onChange={(e) =>
+              setSearchQuery({
+                ...searchQuery,
+                is_closed: Boolean(+e.target.value),
+              })
+            }
+          >
+            <option value="0">Открыт</option>
+            <option value="1">Закрыт</option>
+          </select>
+        </div>
+      )}
     </Search>
   );
 };
--- clients/warmth/src/pages/renters/RentersSelection.jsx
+++ clients/warmth/src/pages/renters/RentersSelection.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import ModalWrapper from "../../components/wrappers/modal/ModalWrapper";
 import Button from "../../components/ui/buttons/base/Button";
+import RenterSearch from "../../forms/renter/RenterSearch";
+import { useRenters } from "../../hooks/useRenters";
 
 const RentersSelection = ({
   rentersList,
@@ -8,42 +10,51 @@ const RentersSelection = ({
   onClose,
   selectedIDs,
   setSelectedIDs,
-  confirmCallback
+  confirmCallback,
 }) => {
-
+  const [searchQuery, setSearchQuery] = useState({
+    title: "",
+    registration_number: ""
+  });
+  const filteredRentersList = useRenters(rentersList, searchQuery);
 
   return (
     <ModalWrapper isVisible={isVisible} closeModal={onClose}>
-      <div style={{ display: "flex", justifyContent: "center", columnGap: "10px" }}>
-        <Button text={"Выделить все"} callback={() => setSelectedIDs(Array.from(rentersList.map(renter => renter.id)))}/>
+      <div
+        style={{ display: "flex", justifyContent: "center", columnGap: "10px" }}
+      >
+        <Button
+          text={"Выделить все"}
+          callback={() =>
+            setSelectedIDs(Array.from(rentersList.map((renter) => renter.id)))
+          }
+        />
         <Button text={"Снять выделен"} callback={() => setSelectedIDs([])} />
         <Button text={"Сформировать"} callback={confirmCallback} />
       </div>
-      <div style={{ minWidth: "80vw", maxHeight: "80vh", overflow: "overlay" }}>
-        {rentersList.map((renter, index) => (
-            <div key={renter.id} style={{ fontSize: "18px", margin: "5px 0" }}>
-              <input
-                size={40}
-                type="checkbox"
-                name=""
-                id={`renter_${renter.id}`}
-                checked={selectedIDs.includes(renter.id)}
-                onChange={() => {
-                  if (selectedIDs.includes(renter.id)) {
-                    setSelectedIDs(selectedIDs.filter(id => id !== renter.id))
-                  } else {
-                    setSelectedIDs([...selectedIDs, renter.id])
-                  }
-                }}
-              />
-              <label
-                style={{ padding: "0 5px" }}
-                htmlFor={`renter_${renter.id}`}
-              >
-                {index}. {renter.name} ({renter.id})
-              </label>
-            </div>
-          ))}
+      <RenterSearch searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <div style={{ minWidth: "80vw", minHeight: "80vh", maxHeight: "80vh", overflow: "overlay" }}>
+        {filteredRentersList.map((renter, index) => (
+          <div key={renter.id} style={{ fontSize: "18px", margin: "5px 0" }}>
+            <input
+              size={40}
+              type="checkbox"
+              name=""
+              id={`renter_${renter.id}`}
+              checked={selectedIDs.includes(renter.id)}
+              onChange={() => {
+                if (selectedIDs.includes(renter.id)) {
+                  setSelectedIDs(selectedIDs.filter((id) => id !== renter.id));
+                } else {
+                  setSelectedIDs([...selectedIDs, renter.id]);
+                }
+              }}
+            />
+            <label style={{ padding: "0 5px" }} htmlFor={`renter_${renter.id}`}>
+              {index}. {renter.name} ({renter.id})
+            </label>
+          </div>
+        ))}
       </div>
     </ModalWrapper>
   );
